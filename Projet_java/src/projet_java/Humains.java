ea0c305ef2df9ac6956c893e8a1e1f8f038a82ad
romain <romain.reina98@gmail.com>
2020-10-05 20:35:47 +0200

    Add Project folder

---
+++ Projet_java/src/projet_java/Humains.java
@@ -0,0 +1,48 @@
+package projet_java;
+
+/**
+ *
+ * @author R
+ */
+public class Humains {
+
+    //Varaible
+   private String premon;
+   private String surnom;
+   private float porte_monnaie;
+   private int cote_de_popularité;
+   private String cri;
+
+   
+   //Constructeur
+    public Humains(String premon, String surnom, float porte_monnaie, int cote_de_popularité, String cri) {
+        this.premon = premon;
+        this.surnom = surnom;
+        this.porte_monnaie = porte_monnaie;
+        this.cote_de_popularité = cote_de_popularité;
+        this.cri = cri;
+    }
+    
+    //Getter
+    public String getPremon() {
+        return premon;
+    }
+
+    public String getSurnom() {
+        return surnom;
+    }
+
+    public float getPorte_monnaie() {
+        return porte_monnaie;
+    }
+
+    public int getCote_de_popularité() {
+        return cote_de_popularité;
+    }
+
+    public String getCri() {
+        return cri;
+    }
+   
+   
+}
