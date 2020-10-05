f6d1fc6de04fdc7c2bdf6f827a654bcb228d5b12
romain <romain.reina98@gmail.com>
2020-10-05 21:08:17 +0200

    Ajout du squelette des  fct dans Humains
    sePresenter, parler, boire, payer, offrirVerre

--- Projet_java/src/projet_java/Humains.java
+++ Projet_java/src/projet_java/Humains.java
@@ -1,5 +1,12 @@
 package projet_java;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
 /**
  *
  * @author R
@@ -43,17 +50,33 @@ public class Humains {
     public String getCri() {
         return cri;
     }
+    
+    // permet de se presenter suivant une phrase predefini
+    public void sePresenter(String phrase, String nom){
+        
+    // si cote elever, on met une phrase de beau gosse, sinon phrase normal
+    // si deja bourré, phrase en consequence
+    System.out.println(nom + ": "+ "bonjours, je m'appel roger er vous ?");
+    System.out.println(nom + ": "+ "salut la compagnie moi c'est ");
+    System.out.println(nom + ": "+ "wesh tes qui, Marcel va te def le cul! ");
+    }
    
    // fonction parler: permet à une personne de parler et enregistrer les log dasn un fichier texte !
-    
     public void parler(String nom,String phrase){
-        System.out.println( nom + ": " + phrase );
-        
+        System.out.println( nom + ": " + phrase ); 
+    }
+    
+    // fct pour boire, action qui permet de changer les variable cote_de_popularité et alcolemie
+    public void boire(){
         
     }
     
+    // 
+    public void payer(){
+    }
     
-    
-    
+    //
+    public void offirVerre(){
+    }
     
 }
